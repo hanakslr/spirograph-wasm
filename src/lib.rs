b3fdcc5e1b872c219051224cb2b718867c8edcc3
Hana Kessler <hanakessler18@gmail.com>
2025-03-17 08:20:58 -0400

    render to canvas instead of returning svg

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,42 @@
+use std::f64;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
+#[wasm_bindgen]
+pub struct Spirograph {
+    ctx: CanvasRenderingContext2d,
+    t: f64,
+}
+
+#[wasm_bindgen]
+impl Spirograph {
+    #[wasm_bindgen(constructor)]
+    pub fn new(canvas_id: &str) -> Self {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document
+            .get_element_by_id(canvas_id)
+            .unwrap()
+            .dyn_into::<HtmlCanvasElement>()
+            .unwrap();
+        let ctx = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()
+            .unwrap();
+
+        ctx.begin_path();
+
+        // Draw a circle
+        ctx.arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
+
+        ctx.stroke();
+
+        Self { ctx, t: 0.0 }
+    }
+}
 #[wasm_bindgen]
 pub fn generate_svg() -> String {
     let mut svg = String::from(
