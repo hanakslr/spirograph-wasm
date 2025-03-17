853578bf0d3e72e475b67ab54a6ed873dd8773e4
Hana Kessler <hanakessler18@gmail.com>
2025-03-17 07:53:38 -0400

    simple spirograph

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,17 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn generate_svg() -> String {
-    let svg = r#"
-        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-        </svg>
-    "#;
-    svg.to_string()
+    let mut svg = String::from(
+        r#"<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+        <path d="M"#,
+    );
+
+    for t in (0..=1000).map(|x| x as f64 * 0.01) {
+        let x = 250.0 + 100.0 * (t * 3.0).cos() + 50.0 * (t * 2.0).cos();
+        let y = 250.0 + 100.0 * (t * 3.0).sin() + 50.0 * (t * 2.0).sin();
+        svg.push_str(&format!("{},{} ", x, y));
+    }
+
+    svg.push_str(r#"" stroke="black" fill="none" stroke-width="2"/></svg>"#);
+    svg
 }
