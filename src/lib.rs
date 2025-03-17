50d17604e5bd80a7eb8697e1f9f08873cf4502bd
Hana Kessler <hanakessler18@gmail.com>
2025-03-17 07:42:17 -0400

    hey look a circle!

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn generate_svg() -> String {
+    let svg = r#"
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+        </svg>
+    "#;
+    svg.to_string()
+}
