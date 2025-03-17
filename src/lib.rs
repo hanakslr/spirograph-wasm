7826eb9073d468ec27817ed539d5348eaf957190
Hana Kessler <hanakessler18@gmail.com>
2025-03-17 16:06:13 -0400

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 use core::f64;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsValue;
-use web_sys::{console, CanvasRenderingContext2d, HtmlCanvasElement};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
 #[wasm_bindgen]
 pub struct Spirograph {
     ctx: CanvasRenderingContext2d,
-    t: f64,
 }
 
 #[wasm_bindgen]
@@ -26,7 +24,7 @@ impl Spirograph {
             .dyn_into::<CanvasRenderingContext2d>()
             .unwrap();
 
-        Self { ctx, t: 0.0 }
+        Self { ctx }
     }
 
     // Helper function to calculate the number of rotations needed
@@ -78,6 +76,7 @@ impl Spirograph {
         let phase_angle = phase_angle.unwrap_or(0.0);
 
         let r_fixed = width / 4.0;
+
         let r = inner_r;
         let p = offset;
 
@@ -93,11 +92,9 @@ impl Spirograph {
         self.ctx.begin_path();
 
         for i in 0..(step_size * rotations) {
-            let t = (2.0 * f64::consts::PI / step_size as f64) * i as f64;
-            let x = ((r_fixed + r) * t.cos())
-                - ((r + p) * (((r_fixed + r) / r) * (t + phase_angle)).cos());
-            let y = (r_fixed + r) * t.sin()
-                - ((r + p) * ((t + phase_angle) * ((r_fixed + r) / r)).sin());
+            let t = (2.0 * f64::consts::PI / step_size as f64) * i as f64 + phase_angle;
+            let x = (r_fixed - r) * t.cos() + p * (((r_fixed - r) / r) * t).cos();
+            let y = (r_fixed - r) * t.sin() + p * (t * ((r_fixed - r) / r)).sin();
 
             self.ctx.line_to(x + width / 2.0, y + height / 2.0);
         }
@@ -105,82 +102,6 @@ impl Spirograph {
         if let Some(color) = stroke_color {
             self.ctx.set_stroke_style_str(&color);
         }
-        self.ctx.stroke();
-    }
-
-    pub fn draw(&mut self) {
-        // This canvas element is set by the dom element in React/JS
-        let width = self.ctx.canvas().unwrap().width() as f64;
-        let height = self.ctx.canvas().unwrap().height() as f64;
-
-        // This just designates resolution
-        let step_size = 3000;
-
-        let r0 = width / 2.0;
-        let n0 = 1.0;
-
-        self.ctx.begin_path();
-        self.ctx.move_to(width, height / 2.0);
-
-        let mut pos_x0 = vec![0.0; step_size];
-        let mut pos_y0 = vec![0.0; step_size];
-
-        for i in 0..step_size {
-            let t = (2.0 * f64::consts::PI / step_size as f64) * i as f64 * n0;
-            let x = r0 * t.cos() + width / 2.0;
-            let y = r0 * t.sin() + height / 2.0;
-            pos_x0[i] = x;
-            pos_y0[i] = y;
-
-            console::log_1(&JsValue::from_str(&format!("0 - x {} y {}", x, y)));
-
-            self.ctx.line_to(x, y);
-        }
-
-        self.ctx.stroke();
-
-        // Now lets draw another one
-        let r1 = width / 8.0;
-        let n1 = 32.0;
-
-        self.ctx.begin_path();
-        self.ctx.move_to(width, height / 2.0);
-
-        let mut pos_x1 = vec![0.0; step_size];
-        let mut pos_y1 = vec![0.0; step_size];
-
-        for i in 0..step_size {
-            let t = (2.0 * f64::consts::PI / step_size as f64) * i as f64 * n1;
-            let x = r1 * t.cos() + pos_x0[i];
-            let y = r1 * t.sin() + pos_y0[i];
-
-            pos_x1[i] = x;
-            pos_y1[i] = y;
-
-            console::log_1(&JsValue::from_str(&format!("1 - x {} y {}", x, y)));
-
-            self.ctx.line_to(x, y);
-        }
-
-        self.ctx.set_stroke_style_str("#0000FF");
-        self.ctx.stroke();
-
-        // And lets draw another one
-        let r1 = width / 21.0;
-        let n1 = 15.0;
-
-        self.ctx.begin_path();
-        self.ctx.move_to(width, height / 2.0);
-
-        for i in 0..step_size {
-            let t = (2.0 * f64::consts::PI / step_size as f64) * i as f64 * n1;
-            let x = r1 * t.cos() + pos_x1[i];
-            let y = r1 * t.sin() + pos_y1[i];
-
-            self.ctx.line_to(x, y);
-        }
-
-        self.ctx.set_stroke_style_str("#800080");
         self.ctx.stroke();
     }
 }
